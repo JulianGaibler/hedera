3782d12782afd6fa6eccbc1757f64e8cc73a7804
JulianWels <julian@jwels.berlin>
2018-10-17 19:02:26 +0200

    :sparkles: FormOptions-Component for Radio and Checkbox lists

---
+++ src/renderer/components/elements/FormOptions.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="itemList">
+		<div v-for="option in config.options" :key="option.value" class="item">
+			<label :for="option.value">
+				<input :type="config.type"
+					:id="option.value"
+					:value="option.value"
+					v-model="selectedOptions"
+					@change="onChange"
+				>
+				{{option.label}}
+			</label>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'formInput',
+	/**
+	 * Data-Props
+	 * {string} value - this will get propagated back to the parent
+	 *
+	 * {string} config.type - checkbox or radio
+	 * {array} config.options - array of options
+	 * {string} config.options.label - label of one option
+	 * {(number|string)} config.options.value - value of one option
+	 */
+	props: [ 'value', 'config' ],
+	data () {
+		return {
+			selectedOptions: this.value
+		}
+	},
+	methods: {
+		onChange: function() {
+			this.$emit('input', this.selectedOptions)
+		}
+	}
+}
+</script>
