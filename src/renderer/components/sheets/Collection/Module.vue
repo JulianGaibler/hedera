2178bd92fa6f738bafe8f7d1a9e09cc733aad7f5
JulianWels <julian@jwels.berlin>
2018-11-19 19:18:48 +0100

    :construction: Work in progress on Module.vue

--- src/renderer/components/sheets/Collection/Module.vue
+++ src/renderer/components/sheets/Collection/Module.vue
@@ -1,11 +1,27 @@
 <template>
 	<div>
-		<div>Main Module</div>
-		<hr>
+		<div class="listMenu">
+			<div>Main Module</div>
+			<hr>
+
+			<div class="dist">
+				<h3>§35b</h3>
+				<h2>Brandstiftung in Internationalen Gewässern</h2>
+				<p>Einwirkung auf die strukturelle Integrität eines Gebäudes durch Feuer in tiefen Gewässern oder luftleeren Räumen welche keinem Staatsgebiet zugeordet sind sofern Arnold keinen Impliziten Anspruch auf sie hat.</p>
+			</div>
+
+			<buttons class="actionBar" :actions="config.actionButtons" />
+		</div>
 	</div>
 </template>
 
 <script>
+import Buttons from '../../elements/Buttons'
+
+import iconClose from '../../../assets/icons/outline-close-24px.svg'
+import typeAnd from '../../../assets/icons/type_and.svg'
+import typeBool from '../../../assets/icons/type_bool.svg'
+import typeExclusive from '../../../assets/icons/type_exclusive.svg'
 
 export default {
 	name: 'module',
@@ -18,11 +34,36 @@ export default {
 	 * {array} data.callOnMount.args - method arguments
 	 */
 	data: function () {
+		let actionButtons = {
+			left: [{
+				icon: iconClose,
+				callback: () => this.sheet.closeSelf(this.sheet)
+			}],
+			right: [
+				{
+					icon: typeAnd,
+					class: 'selected',
+					callback: () => {}
+				},
+				{
+					icon: typeBool,
+					callback: () => {}
+				},
+				{
+					icon: typeExclusive,
+					callback: () => {}
+				},
+			]
+		}
 		return {
-
+			config: {
+				actionButtons,
+			},
 		}
 	},
-	components: { },
+	components: {
+		Buttons
+	},
 	methods: { }
 }
 </script>
